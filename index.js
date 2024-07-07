a16fbdc83d1b657010a93c7cb66cbd6db8bbe47d
CrxstianEscobar <cristianyhacson2008@gmail.com>
2024-07-07 10:02:11 -0500

    Update index.js
    𝙎𝙝𝙖𝙙𝙤𝙬 𝘽𝙤𝙩-𝙎𝙓

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('The Mystic\nBot', {
+say('Shadow Bot\nWa SX', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot creado por Bruno Sobrino`, {
+say(`Created by Cristian Escobar`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
