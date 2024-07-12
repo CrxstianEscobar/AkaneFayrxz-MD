18084ac0a37cbc219cd63f4014071ceb257de2c9
CrxstianEscobar <cristianyhacson2008@gmail.com>
2024-07-12 18:47:43 -0500

    Update menu-freefire.js

--- plugins/menu-freefire.js
+++ plugins/menu-freefire.js
@@ -2,7 +2,6 @@ import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
   try {
     const pp = `imagen11`;
-    //const vn = './media/La biblia.mp3';
     const d = new Date(new Date + 3600000);
     const locale = 'es';
     const week = d.toLocaleDateString(locale, {weekday: 'long'});
@@ -18,7 +17,42 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `MENU EN DESARROLLO GAY EL QUE LO LEA`.trim();
+    const str = `> ⓘ ¡Hola!, ${taguser}
+> ${date}
+*˚₊·˚₊· ͟͟͞͞➳❥ _Shadow Bot-SX_*
+*☆═━┈◈ ╰ 1.4.0 SB ╯ ◈┈━═☆*
+*│* 
+*╰ ˚₊·˚₊· ͟͟͞͞➳❥ _By Cristian_*
+*⊰᯽⊱┈──╌•|* ⊱✿⊰ *|•╌──┈⊰᯽⊱*
+*╭┄⊰* *MENÚ FREE FIRE*
+*│* _.fem4_
+*│* _.fem6_
+*│* _.feminterna4_
+*│* _.feminterna6_
+*│* _.masc4_
+*│* _.masc6_
+*│* _.mascinterna4_
+*│* _.mascinterna6_
+*│* _.mixto4_
+*│* _.mixto6_
+*│* _.mixtointerna4_
+*│* _.mixtointerna6_
+*│* _.lista4_
+*│* _.apos_
+*│* _.mapa_
+*│* _.ficha_
+*│* _.nexterra_
+*│* _.bermuda_
+*│* _.kalahari_
+*│* _.alpes_
+*│* _.agendasemanal_
+*│* _.buscarvsmasc_
+*│* _.vsmasc_
+*│* _.buscarvsfem_
+*│* _.vsfem_
+*│* _.buscarvsmixto_
+*│* _.vsmixto_
+*╰─────────────────┄⊰*`.trim();
     if (m.isGroup) {
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
       await conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
@@ -41,4 +75,4 @@ function clockString(ms) {
   const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
   const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
   return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
-}
+  }
