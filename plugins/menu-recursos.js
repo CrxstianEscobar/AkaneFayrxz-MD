453c320517dd58f286da920007d65bbf82141513
CrxstianEscobar <cristianyhacson2008@gmail.com>
2024-07-13 18:23:26 -0500

    Update menu-recursos.js

--- plugins/menu-recursos.js
+++ plugins/menu-recursos.js
@@ -16,7 +16,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `> ⓘ ¡Hola!, ${taguser}
-> ${date} ${week}
+> ${date}
 *˚₊·˚₊· ͟͟͞͞➳❥ _Shadow Bot-SX_*
 *☆═━┈◈ ╰ 1.4.0 SB ╯ ◈┈━═☆*
 *│* 
