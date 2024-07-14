a1b19143d3832e5925e03a09957809305fd0dee2
CrxstianEscobar <cristianyhacson2008@gmail.com>
2024-07-14 13:30:22 -0500

    Create ff-fem4.js

---
+++ plugins/ff-fem4.js
@@ -0,0 +1,30 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.fem4);
+};
+handler.command = /^(fem4|fem4vs4)$/i;
+export default handler;
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+global.fem4 = `
+𝟰 𝗩𝗘𝗥𝗦𝗨𝗦 𝟰
+
+𝐇𝐎𝐑𝐀𝐑𝐈𝐎
+🇵🇪 𝐏𝐄𝐑𝐔 : 
+🇦🇷 𝐀𝐑𝐆𝐄𝐍𝐓𝐈𝐍𝐀 : 
+🇨🇱 𝐂𝐇𝐈𝐋𝐄 :  
+¬ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒 𝐏𝐑𝐄𝐒𝐄𝐍𝐓𝐄𝐒
+
+🌸 ┇  
+🌸 ┇ 
+🌸 ┇ 
+🌸 ┇ 
+
+ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
+
+🌸 ┇ 
+🌸 ┇
+
+*◤By : Cristian ◢*
+`;
