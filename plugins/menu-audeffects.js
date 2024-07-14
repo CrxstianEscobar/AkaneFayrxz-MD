ad85ee023ba7b4cc8f8eef48ecdeb97ab1a05fde
CrxstianEscobar <cristianyhacson2008@gmail.com>
2024-07-13 23:34:56 -0500

    Create menu-audeffects.js

---
+++ plugins/menu-audeffects.js
@@ -0,0 +1,61 @@
+import fetch from 'node-fetch';
+
+
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+
+  try {
+    const pp = imagen2;
+    const d = new Date(new Date + 3600000);
+    const locale = 'es';
+    const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
+    const user = global.db.data.users[m.sender];
+    const more = String.fromCharCode(8206);
+    const readMore = more.repeat(850);
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+    const document = doc[Math.floor(Math.random() * doc.length)];
+    const str = `> ⓘ ¡Hola!, ${taguser}
+> ${date}
+*˚₊·˚₊· ͟͟͞͞➳❥ _Shadow Bot-SX_*
+*☆═━┈◈ ╰ 1.4.0 SB ╯ ◈┈━═☆*
+*│* 
+*╰ ˚₊·˚₊· ͟͟͞͞➳❥ _By Cristian_*
+*⊰᯽⊱┈──╌•|* ⊱✿⊰ *|•╌──┈⊰᯽⊱*
+*╭┄⊰ =͟͟͞͞ MENU AUD EFFECTS*
+*│* ⓘ Responde a un audio.
+*│* დ _.bass_
+*│* დ _.blown_
+*│* დ _.deep_
+*│* დ _.earrape_
+*│* დ _.fast_
+*│* დ _.fat_
+*│* დ _.nightcore_
+*│* დ _.reverse_
+*│* დ _.robot_
+*│* დ _.slow_
+*│* დ _.smooth_
+*│* დ _.tupai_
+*╰─────────────────┄⊰*`.trim();
+    if (m.isGroup) {
+      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+    } else {
+      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+    }
+  } catch {
+    conn.reply(m.chat,'╰⊱❌⊱ *_ERROR_* ⊱❌⊱╮\n\n*_EL MENÚ ESTÁ FALLANDO INTENTE DE NUEVO MÁS TARDE_*', m);
+  }
+};
+handler.command = /^(audioefectos|audefectos|menuadefectos|audefects|menuefeaud|menuaudeffects)$/i;
+handler.exp = 50;
+handler.fail = null;
+export default handler;
+function clockString(ms) {
+  const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000);
+  const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
+  const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
+  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
+}
