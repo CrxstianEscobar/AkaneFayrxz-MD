eccbae6d2dba15ecd0dbc3ee43dbddff458c994e
CrxstianEscobar <cristianyhacson2008@gmail.com>
2024-07-18 14:17:46 -0500

    Update descargas-mega.js

--- plugins/descargas-mega.js
+++ plugins/descargas-mega.js
@@ -1,33 +1,40 @@
 import { File } from "megajs";
-import mime from 'mime-types';
+import path from "path";
 
 let handler = async (m, { conn, args, usedPrefix, text, command }) => {
     try {
-        if (!text) return m.reply(`Ingresa un enlace de Mega`);
+        if (!text) return m.reply(`𝙄 𝙉 𝙁 𝙊 𝙍 𝙈 𝘼 𝘾 𝙄 𝙊 𝙉\n\n𝙁𝙤𝙧𝙢𝙖𝙩𝙤 𝙞𝙣𝙘𝙤𝙧𝙧𝙚𝙘𝙩𝙤\n\n» 𝙐𝙨𝙚 𝙚𝙡 𝙘𝙤𝙢𝙖𝙣𝙙𝙤:\n𝘌𝘫𝘦𝘮𝘱𝘭𝘰: .𝘮𝘦𝘨𝘢 <𝘭𝘪𝘯𝘬>`);
+
         const file = File.fromURL(text);
         await file.loadAttributes();
-        const fileExtension = file.name.split('.').pop().toLowerCase();
-        const mimeType = mime.lookup(fileExtension);
-        let caption = `${eg}
-┃ 𓃠 *MEGA - DESCARGAS*
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 💫 𝙉𝙊𝙈𝘽𝙍𝙀 | 𝙉𝘼𝙈𝙀
-┃ ${file.name}
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 💪 𝙋𝙀𝙎𝙊 |  𝙎𝙄𝙕𝙀
-┃ ${formatBytes(file.size)}
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 🚀 𝙏𝙄𝙋𝙊 | 𝙏𝙔𝙋𝙀
-┃ ${mimeType}
-┃
-┃ 1.4.0`.trim()
-conn.reply(m.chat,caption, m)
-        if (file.size >= 1800000000 && !file.directory) return m.reply('Error: El archivo es muy pesado');
+
+        if (file.size >= 300000000) return m.reply('Error: El archivo es grande (Maximo tamaño: 300MB)');
+
+        const downloadingMessage = `𝘾𝙖𝙧𝙜𝙖𝙣𝙙𝙤...`;
+        m.reply(downloadingMessage);
+
+        const caption = `𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖 𝙚𝙭𝙞𝙩𝙤𝙨𝙖....\n\n» 𝙉𝙤𝙢𝙗𝙧𝙚: ${file.name}\n» 𝙋𝙚𝙨𝙤: ${formatBytes(file.size)}\n» 𝙁𝙚𝙘𝙝𝙖: ${fecha}`;
+
         const data = await file.downloadBuffer();
-        await conn.sendFile(m.chat, data, file.name, null, m, null, { mimeType, asDocument: true });
+
+        const fileExtension = path.extname(file.name).toLowerCase();
+        const mimeTypes = {
+            ".mp4": "video/mp4",
+            ".pdf": "application/pdf",
+            ".zip": "application/zip",
+            ".rar": "application/x-rar-compressed",
+            ".7z": "application/x-7z-compressed",
+            ".jpg": "image/jpeg",
+            ".jpeg": "image/jpeg",
+            ".png": "image/png",
+        };
+
+        let mimetype = mimeTypes[fileExtension] || "application/octet-stream";
+
+        await conn.sendFile(m.chat, data, file.name, caption, m, null, { mimetype, asDocument: true });
 
     } catch (error) {
-        return m.reply(`ERROR!`);
+        return m.reply(`Error: ${error.message}`);
     }
 }
 
@@ -38,8 +45,10 @@ export default handler
 
 function formatBytes(bytes) {
     if (bytes === 0) return '0 Bytes';
+
     const k = 1024;
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
+
     return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
 }
