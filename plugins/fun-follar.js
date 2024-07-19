29beef936bfa385232c1e350f1700db3c878dce4
CrxstianEscobar <cristianyhacson2008@gmail.com>
2024-07-19 16:47:47 -0500

    Update fun-follar.js

--- plugins/fun-follar.js
+++ plugins/fun-follar.js
@@ -2,15 +2,13 @@
 let handler = async (m, { conn, command, text }) => {
 if (!text) throw `*Ingrese el @ o el nombre de la persona que quieras saber si te puedes ${command.replace('how', '')}*`
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-conn.reply(m.chat, `
-🥵 *ACABAS DE FOLLARTELA!*🥵
+conn.reply(m.chat, `ⓘ _Acabas de follar a ${text} poniendola en 4 como una perra, la hiciste mamar tu vrg haciendola garganta profunda, le reventaste la vagina mientras te gemia, y posiblemente haya quedado embarazada. 🥵_
 
-*Acabas de follar a ${text} poniendola en 4 por el culo y vaginx mientras te gemía como una putita:*\n> Aahh..,Aaah, sigue, no pares, no pares papi, meteme bien duroo...\n*Y la dejaste tan abierta que no puede ni caminar la puta!*
+> Que rico papi, dale más duro eso bb que rico, meteme bien duro, hazme tu puta!..
 
-*${text}* 
-🤤🥵 *¡YA TE METIERON LA VRG!* 🥵🤤`, null, { mentions: [user] })
+_${text} ya estás contenta como te dejaron la vagina y boca llena de leche? 🦊_`, null, { mentions: [user] })
 }
 
-handler.command = /^(Follar|violar|kchar)/i
+handler.command = /^(Follar|coger|violar|kchar)/i
 handler.fail = null
 export default handler
