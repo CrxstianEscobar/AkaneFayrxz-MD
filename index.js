d95ed6ca9482948371785cf2199913a33a165e5e
CrxstianEscobar <cristianyhacson2008@gmail.com>
2024-07-26 15:29:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('Shadow Bot\nWa SX', {
+say('Shadow\nBot\nSX', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Created by Cristian Escobar`, {
+say(`By Cristian Escobar`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
