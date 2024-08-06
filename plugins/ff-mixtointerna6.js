5ec30ab048f79e1a6f2f865fcc11152424078e7a
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-08-06 10:03:33 -0500

    Add files via upload

---
+++ plugins/ff-mixtointerna6.js
@@ -0,0 +1,31 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.mixtointerna6);
+};
+handler.command = /^(mixtointerna6|internamixto6vs6)$/i;
+export default handler;
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+global.mixtointerna6 = `ㅤㅤㅤ *_INTERNA MIXTA_*
+╭─────━━━━────── •••
+»  _*⏰Hora:* 00🇵🇪 / 02🇦🇷_
+»  _*☁️Modalidad:* 6vs6_
+╰◣◥◣◥◤◢◤◢◣◥◣◥◤
+ㅤ _ʚ Equipo 1:_ ᭡
+❄️ • 
+❄️ • 
+❄️ • 
+❄️ • 
+❄️ • 
+❄️ • 
+ㅤ _ʚ Equipo 2:_ ᭡
+🔥 • 
+🔥 •
+🔥 • 
+🔥 • 
+🔥 • 
+🔥 • 
+
+_ঔ Organiza: <nombre>_
+· · ────── ·•· ────── · ·`;
